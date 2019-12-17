5542ed804c80cd51403e1a459bc676eb8f24f919
Alexander Fahnehjelm <jfah0001@student.monash.edu>
2019-12-17 14:22:41 +0100

    toggle button

--- js/new-age.js
+++ js/new-age.js
@@ -64,3 +64,22 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+function toggleDiv(divid)
+  {
+
+    varon = divid + 'on';
+    varoff = divid + 'off';
+ 
+    if(document.getElementById(varon).style.display == 'block')
+    {
+    document.getElementById(varon).style.display = 'none';
+    document.getElementById(varoff).style.display = 'block';
+    }
+
+    else
+    {
+    document.getElementById(varoff).style.display = 'none';
+    document.getElementById(varon).style.display = 'block'
+    }
+}
